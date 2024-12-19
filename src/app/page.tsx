67792203e798d9ad223f5ce6e5cbf1abd3420fdf
jamiu Babatunde <muhammaduljamih@gmail.com>
2024-12-19 17:33:37 +0100

    add audio list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,4 @@
-<<<<<<< HEAD
-
-
-
-import Aside from "@/components/aside";
-import BannerSection from "@/components/banner-section";
-import FeatureSection from "@/components/feature-section";
-import HeroSection from "@/components/hero-section";
-import RecentPosts from "@/components/recent-posts";
-=======
 import AudioList from "@/components/audio/audio-list";
->>>>>>> 92c09a4 (merged)
 
 export default function Home() {
   return <div className="max-w-screen relative">
