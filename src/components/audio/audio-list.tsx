4acea4ea25e0a554ee705f9712d81688d297e2e8
jamiu Babatunde <muhammaduljamih@gmail.com>
2024-12-20 12:50:28 +0100

    refactor audio components and add audio playlist

--- src/components/audio/audio-list.tsx
+++ src/components/audio/audio-list.tsx
@@ -5,28 +5,33 @@ import Pagination from "./audio-pagination";
 
 const AudioList: React.FC = () => {
   return (
+    // Container for the audio list component
     <div className="mx-10 my-5">
       <div className="flex">
-        <div className="flex flex-wrap gap-7">
-          {audioList.map((audio, index) => (
-            <AudioCard
-              key={index}
-              title={audio.title}
-              desc={audio.desc}
-              body={audio.body}
-              author={audio.author}
-              date={audio.date}
-            />
-          ))}
+        <div>
+          {/* Container for the audio cards */}
+          <div className="flex flex-wrap gap-7">
+            {audioList.map((audio, index) => (
+              <AudioCard
+                key={index}
+                title={audio.title}
+                desc={audio.desc}
+                body={audio.body}
+                author={audio.author}
+                date={audio.date}
+              />
+            ))}
+          </div>
+
+          {/* Pagination Start */}
+          <div>
+            <Pagination />
+          </div>
         </div>
+        {/* Container for the recommended aside component */}
         <div>
           <RecommendedAside />
         </div>
-      </div>
-
-      {/* Pagination Start */}
-      <div>
-        <Pagination/>
       </div>
     </div>
   );
--- src/components/audio/audio-pagination.tsx
+++ src/components/audio/audio-pagination.tsx
@@ -5,22 +5,24 @@ import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 export default function Pagination() {
   return (
     // Container for the pagination component
-    <div className="mt-10 flex items-center justify-center space-x-2">
+    <div className="mt-10 flex items-center justify-center space-x-3">
       {/* Previous button with left arrow icon */}
-      <button className="flex items-center rounded border px-4 py-2">
-        <FaArrowLeft className="" />
+      <button className="flex items-center rounded-xl border px-4 py-[12px]">
+        <FaArrowLeft className="text-gray-700" />
       </button>
-      {/* Page number 1 to 5*/}
-      <span className="rounded border bg-[#041D5B] px-4 py-2 text-white">
+
+      {/* Page number 1 to 5 */}
+      <span className="rounded-xl border bg-[#041D5B] px-4 py-2 text-white">
         1
       </span>
-      <span className="rounded border px-4 py-2">2</span>
-      <span className="rounded border px-4 py-2">3</span>
-      <span className="rounded border px-4 py-2">4</span>
-      <span className="rounded border px-4 py-2">5</span>
+      <span className="px-4 py-2 underline">2</span>
+      <span className="px-4 py-2 underline">3</span>
+      <span className="px-4 py-2 underline">4</span>
+      <span className="px-4 py-2 underline">5</span>
+
       {/* Next button with right arrow icon */}
-      <button className="rounded border px-4 py-2">
-        <FaArrowRight className="" />
+      <button className="rounded-xl border px-4 py-[12px]">
+        <FaArrowRight className="text-gray-700" />
       </button>
     </div>
   );
