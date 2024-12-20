97d34badfb2fafc8b0094f9630a7090e5f37f6c5
jamiu Babatunde <muhammaduljamih@gmail.com>
2024-12-20 09:36:21 +0100

    modified pagination

--- src/components/audio/audio-pagination.tsx
+++ src/components/audio/audio-pagination.tsx
@@ -1,5 +1,27 @@
+import React from "react";
+
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
+
 export default function Pagination() {
   return (
-    <div>audio-pagination</div>
-  )
+    // Container for the pagination component
+    <div className="mt-10 flex items-center justify-center space-x-2">
+      {/* Previous button with left arrow icon */}
+      <button className="flex items-center rounded border px-4 py-2">
+        <FaArrowLeft className="" />
+      </button>
+      {/* Page number 1 to 5*/}
+      <span className="rounded border bg-[#041D5B] px-4 py-2 text-white">
+        1
+      </span>
+      <span className="rounded border px-4 py-2">2</span>
+      <span className="rounded border px-4 py-2">3</span>
+      <span className="rounded border px-4 py-2">4</span>
+      <span className="rounded border px-4 py-2">5</span>
+      {/* Next button with right arrow icon */}
+      <button className="rounded border px-4 py-2">
+        <FaArrowRight className="" />
+      </button>
+    </div>
+  );
 }
